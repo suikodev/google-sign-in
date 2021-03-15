0a06a9ea8574c49afe034ec26fe39985176ace63
nacht <nacht42@riseup.net>
2021-03-15 09:29:55 +0800

    feat(src/googlesignin): integrate gapi

--- src/GoogleSignIn.js
+++ src/GoogleSignIn.js
@@ -22,16 +22,46 @@ export class GoogleSignIn extends LitElement {
     super();
     this.title = 'Hey there';
     this.counter = 5;
-  }
-
-  __increment() {
-    this.counter += 1;
+    this.script = this.script.bind(this);
+    this.onSignIn = this.onSignIn.bind(this);
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <div class="g-signin2" data-onsuccess=${this.onSignIn}></div>
+      ${this.script()}
     `;
   }
+
+  onLoad() {
+    console.log(gapi);
+    gapi.load("client:auth2", () => {
+      gapi.auth2.init({
+        client_id: "828207170600-ba2r0cpgqu4sknf8ci46itbl8ss6doeb.apps.googleusercontent.com"
+      });
+    });
+  }
+
+  script() {
+    const script = document.createElement('script');
+    script.onload = this.onLoad.bind(this);
+    script.src = 'https://apis.google.com/js/platform.js';
+    return script;
+  }
+
+  onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    const profile = googleUser.getBasicProfile();
+    console.log(`ID: ${  profile.getId()}`); // Don't send this directly to your server!
+    console.log(`Full Name: ${  profile.getName()}`);
+    console.log(`Given Name: ${  profile.getGivenName()}`);
+    console.log(`Family Name: ${  profile.getFamilyName()}`);
+    console.log(`Image URL: ${  profile.getImageUrl()}`);
+    console.log(`Email: ${  profile.getEmail()}`);
+
+    // The ID token you need to pass to your backend:
+    const {id_token} = googleUser.getAuthResponse();
+    console.log(`ID Token: ${  id_token}`);
+  }
+
 }
